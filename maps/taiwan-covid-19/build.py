382c589c509ff49bcc10c2ecc327ae711ed1909f
ythlev <60928242+ythlev@users.noreply.github.com>
2020-03-05 02:23:46 +0800

    update

--- maps/taiwan-covid-19/build.py
+++ maps/taiwan-covid-19/build.py
@@ -1,21 +1,24 @@
-import io, csv, urllib.request, math
+import urllib.request, csv, io, math
 
-subdivisions = {}
-with open("subdivisions.csv", newline = "", encoding = "utf-8") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        subdivisions[row["COUNTYNAME"]] = {"cases": 0}
+subdivisions= [
+    "新北市", "台北市", "桃園市", "台中市", "台南市", "高雄市", "宜蘭縣", "新竹縣", "苗栗縣", "彰化縣", "南投縣",
+    "雲林縣", "嘉義縣", "屏東縣", "台東縣", "花蓮縣", "澎湖縣", "基隆市", "新竹市", "嘉義市", "金門縣", "連江縣"
+]
+
+main = {}
+for x in subdivisions:
+    main[x] = {"cases": 0}
 
 with urllib.request.urlopen("https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.csv") as response:
     reader = csv.DictReader(io.TextIOWrapper(response, encoding = 'utf-8'), delimiter=',')
     for row in reader:
-        for k, v in subdivisions.items():
+        for k, v in main.items():
             if row["縣市"] == k:
-                subdivisions[k]["cases"] += int(row["確定病例數"])
+                main[k]["cases"] += int(row["確定病例數"])
                 break
 
 list = []
-for k, v in subdivisions.items():
+for k, v in main.items():
     list.append(v["cases"])
 
 min = min(list)
@@ -42,21 +45,21 @@ with open("template.svg", "r", newline = "", encoding = "utf-8") as f_in:
     with open("output.svg", "w", newline = "", encoding = "utf-8") as f_out:
         for row in f_in:
             written = False
-            for k, v in subdivisions.items():
+            for k, v in main.items():
                 if row.find(k) > 0:
                     i = 0
                     while v["cases"] >= thresholds[i]:
                         i += 1
                     i -= 1
-                    subdivisions[k]["threshold met"] = thresholds[i]
-                    subdivisions[k]["colour"] = colours[i]
-                    f_out.write(row.replace('id="%s"' % k, 'style="fill:%s"' % subdivisions[k]["colour"]))
+                    main[k]["threshold met"] = thresholds[i]
+                    main[k]["colour"] = colours[i]
+                    f_out.write(row.replace('id="%s"' % k, 'style="fill:%s"' % main[k]["colour"]))
                     written = True
                     break
             if written == False:
                 f_out.write(row)
 
-for k, v in subdivisions.items():
+for k, v in main.items():
     print(k, v)
 
 print("Total cases: ", sum(list))
