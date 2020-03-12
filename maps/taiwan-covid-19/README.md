5e4a5a56504db315a2c804616f052082c3e41a9e
ythlev <60928242+ythlev@users.noreply.github.com>
2020-03-12 18:00:02 +0800

    density

--- maps/taiwan-covid-19/README.md
+++ maps/taiwan-covid-19/README.md
@@ -1,2 +1,3 @@
 # Sources
 * [衛生福利部疾病管制署 最新資料](https://data.gov.tw/dataset/118038)
+* [內政部戶政司全球資訊網 最新資料](https://gis.ris.gov.tw/dashboard.html?key=B01)
--- maps/taiwan-covid-19/build.py
+++ maps/taiwan-covid-19/build.py
@@ -1,14 +1,16 @@
 # 張嘉桓 製
-import urllib.request, json, math
-
-places = [
-    "新北市", "台北市", "桃園市", "台中市", "台南市", "高雄市", "宜蘭縣", "新竹縣", "苗栗縣", "彰化縣", "南投縣",
-    "雲林縣", "嘉義縣", "屏東縣", "台東縣", "花蓮縣", "澎湖縣", "基隆市", "新竹市", "嘉義市", "金門縣", "連江縣"
-]
+import csv, urllib.request, json, math
 
 main = {}
-for place in places:
-    main[place] = {"cases": 0}
+
+with open("populations.csv", newline = "", encoding = "utf-8-sig") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if row["區域別"] != "總計":
+            main[row["區域別"].replace("臺", "台")] = {
+                "cases": 0,
+                "population": int(row["總計"].replace(",", ""))
+            }
 
 with urllib.request.urlopen("https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_19CoV.json") as response:
     data = json.loads(response.read())
@@ -18,22 +20,29 @@ with urllib.request.urlopen("https://od.cdc.gov.tw/eic/Weekly_Age_County_Gender_
                 main[place]["cases"] += int(row["確定病例數"])
                 break
 
+for attrs in main.values():
+    attrs["density"] = attrs["cases"] / attrs["population"]
+
 list = []
 for attrs in main.values():
-    list.append(attrs["cases"])
+    list.append(attrs["density"])
 list.sort()
 
-high = list[-2]
-if list[1] == 0:
-    low = 1
-else:
+low = 0
+if list[1] > 0:
     low = list[1]
+else:
+    for value in list:
+        if value > 0:
+            low = value
+            break
+high = list[-2]
 
 step = math.log(high / low) / 5
 
 thresholds = [0, 0, 0, 0, 0, 0]
 for i in range(5):
-    thresholds[i + 1] = round(low * math.exp(step * i))
+    thresholds[i + 1] = low * math.exp(step * i)
 
 colours = ["#fee5d9","#fcbba1","#fc9272","#fb6a4a","#de2d26","#a50f15"]
 
@@ -45,7 +54,7 @@ with open("template.svg", "r", newline = "", encoding = "utf-8") as file_in:
                 if row.find(place) > -1:
                     i = 0
                     while i < 5:
-                        if attrs["cases"] >= thresholds[i + 1]:
+                        if attrs["density"] >= thresholds[i + 1]:
                             i += 1
                         else:
                             break
@@ -60,6 +69,14 @@ with open("template.svg", "r", newline = "", encoding = "utf-8") as file_in:
 for place, attrs in main.items():
     print(place, attrs)
 
-print("Total cases:", sum(list))
+cases = []
+for attrs in main.values():
+    cases.append(attrs["cases"])
+print("Total cases:", sum(cases))
+
 print("Colours:", colours)
-print("Thresholds:", thresholds, "Max:", max(list))
+
+thresholds_rescaled = []
+for value in thresholds:
+    thresholds_rescaled.append(round(1000000 * value, 2))
+print("Thresholds (ppm):", thresholds_rescaled, "Max:", round(1000000 * max(list), 2), "Second:", round(1000000 * high, 2))
