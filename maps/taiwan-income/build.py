e0d9e269cd78f07a2cd759ea6344b4f7f4dbf535
ythlev <60928242+ythlev@users.noreply.github.com>
2020-03-25 16:48:11 +0800

    update

--- maps/taiwan-income/build.py
+++ maps/taiwan-income/build.py
@@ -12,19 +12,19 @@ with open("105_165-9.csv", newline = "", encoding = "utf-8") as file:
     for row in reader:
         if (row["﻿縣市"] + row["鄉鎮市區"]) in main:
             main[row["﻿縣市"] + row["鄉鎮市區"]].append(float(row["中位數"]))
-        values.append(float(row["中位數"]))
 
 for place in main:
     main[place] = statistics.median(main[place])
+    values.append(main[place])
 
-colour = ['#f2f0f7','#dadaeb','#bcbddc','#9e9ac8','#756bb1','#54278f']
-
-step = math.sqrt(0.2 * statistics.median(values)) / 3
+q = statistics.quantiles(values, n = 100)
+step = math.sqrt(statistics.mean(values) - q[0]) / 3
 
 threshold = [0, 0, 0, 0, 0, 0]
-for i in range(1,6):
-    threshold[i] = math.pow(i * step, 2) + 0.8 * statistics.median(values)
+for i in range(1, 6):
+    threshold[i] = math.pow(i * step, 2) + q[0]
 
+colour = ['#f2f0f7','#dadaeb','#bcbddc','#9e9ac8','#756bb1','#54278f']
 with open("template.svg", "r", newline = "", encoding = "utf-8") as file_in:
     with open("result.svg", "w", newline = "", encoding = "utf-8") as file_out:
         for row in file_in:
