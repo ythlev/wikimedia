c91baf6efbba6d13b0e79c5f0cd417ecdf6ad66d
ythlev <60928242+ythlev@users.noreply.github.com>
2020-04-19 10:49:53 +0800

    ce

--- maps/elections-tw-database/run.py
+++ maps/elections-tw-database/run.py
@@ -2,11 +2,19 @@ import argparse, csv, json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-f", "--file")
-parser.add_argument("-v", "--verify", action = "store_const", const = True)
+parser.add_argument("-c", "--commit", action = "store_const", const = True)
 args = vars(parser.parse_args())
 main = {}
 
-with open("el.json", "w+", newline = "", encoding = "utf-8") as file:
+if args["commit"] == True:
+    import os
+    os.replace("el-preview.json", "el.json")
+    quit()
+
+with open("el.json", newline = "") as file:
     el = json.loads(file.read())
-    el["description"] = "Database for elections in Taiwan"
+
+el["description"] = "Database for elections in Taiwan"
+
+with open("el-preview.json", newline = "") as file:
     file.write(json.dumps(el, indent = 2))
