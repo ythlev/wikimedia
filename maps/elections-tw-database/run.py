37c3b24110529dc17054d4c67e85b8787602eebc
ythlev <60928242+ythlev@users.noreply.github.com>
2020-04-19 12:05:07 +0800

    ce

--- maps/elections-tw-database/run.py
+++ maps/elections-tw-database/run.py
@@ -1,8 +1,9 @@
 import argparse, csv, json
-from datetime import datetime
+from datetime import datetime, date
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-e", "--election")
+parser.add_argument("-d", "--date")
 parser.add_argument("-f", "--file")
 parser.add_argument("-c", "--commit", action = "store_const", const = True)
 args = vars(parser.parse_args())
@@ -18,9 +19,17 @@ with open("el.json", newline = "", encoding = "utf-8") as file:
 
 el["lastUpdatedAt"] = datetime.now().isoformat()
 
+if args["election"] != None:
+    d = {
+        "name": args["election"],
+        "date": str(date.fromisoformat(args["date"]))
+    }
+    if d not in el["elections"]:
+        el["elections"].append(d)
+    el["elections"].sort(key = lambda d : d["date"])
+
 if args["file"] != None:
     pass
-
 
 with open("el-preview.json", "w", newline = "", encoding = "utf-8") as file:
     file.write(json.dumps(el, indent = 2, ensure_ascii = False))
